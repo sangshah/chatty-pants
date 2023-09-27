e393e23b72fa9d1157b66ff957a4e464a99cf05a
Sangeeta Shah <76058791+sangshah@users.noreply.github.com>
2023-09-27 16:15:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from hugchat import hugchat
 from hugchat.login import Login
 
-//st.title('🎈 ChattyPants - AI Chatbot')
+#st.title('🎈 ChattyPants - AI Chatbot')
 st.set_page_config(page_title="🤗💬 ChattyPants")
 
 
